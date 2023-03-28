cbb11b6295fd444de793fcc8170a76a1a16c8391
zhujinlong <1445824783@qq.com>
2023-03-28 22:17:07 +0800

    访问设备

--- electron/day01/example03/main.js
+++ electron/day01/example03/main.js
@@ -31,11 +31,130 @@ const createWindow = ()=>{
     win.webContents.send('bluetooth-pairing-request',details)
   })
 
+  //  **********************访问HID设备*************************
+
+  // 调用 navigator.hid.requestDevice 并选择高清设备，将会触发会话内的 select-hid-device 事件
+  win.webContents.session.on('select-hid-device',(event,details,callback)=>{
+    // hid-device-added/removed 两种事件可以处理高清设备的插拔
+    win.webContents.session.on('hid-device-added',(event,device)=>{
+      console.log('hid-device-added Frist width ',device);
+    })
+    win.webContents.session.on('hid-device-removed',(event,device)=>{
+      console.log('hid-device-removed Frist width ',device);
+    })
+    event.preventDefault()
+    if(details.deviceList && details.deviceList.length > 0){
+      callback(details.deviceList[0].deviceId)
+    }
+  })
+
+  // 该函数用于禁用特定来源的 HID 访问
+  win.webContents.session.setPermissionCheckHandler((webContents,permission,requestingOrigin,details)=>{
+    if(permission === 'hid' && details.securityOrigin === 'file:///'){
+      return true
+    }
+  })
+ // 第一次调用 navigator.hid.requestDevice 前，可以通过该函数给予设备默认权限
+  win.webContents.session.setDevicePermissionHandler((details)=>{
+    if(details.deviceType === 'hid' && details.origin === 'file:///'){
+      return true
+    }
+  })
 
 
-  win.webContents.openDevTools()
+
+
+  //  **********************访问串口设备 如：USB 蓝牙*************************
+
+  // 调用 navigator.serial.requestPort ，将会触发会话内的 select-serial-port 事件
+  win.webContents.session.on('select-serial-port',(event,portList,webContents,callback)=>{
+    // serial-port-added/removed 两种事件可以处理串口设备的插拔
+    win.webContents.session.on('serial-port-added',(event,port)=>{
+      console.log('serial-port-added Frist width ',port);
+    })
+    win.webContents.session.on('serial-port-removed',(event,port)=>{
+      console.log('serial-port-removed Frist width ',port);
+    })
+    event.preventDefault()
+    if(portList && portList.length > 0){
+      callback(portList[0].portId)
+    }else{
+      callback('')
+    }
+  })
+
+  // 该函数用于禁用特定来源的串口访问
+  win.webContents.session.setPermissionCheckHandler((webContents,permission,requestingOrigin,details)=>{
+    if(permission === 'serial' && details.securityOrigin === 'file:///'){
+      return true
+    }
+    return false
+  })
+ // 第一次调用 navigator.serial.requestPort 前，可以通过该函数给予设备默认权限
+  win.webContents.session.setDevicePermissionHandler((details)=>{
+    if(details.deviceType === 'serial' && details.origin === 'file:///'){
+      return true
+    }
+    return false
+  })
+
+  //  **********************访问 Web USE*************************
+  let grantedDeviceThroughPermHandler
+  // 调用 navigator.serial.requestPort ，将会触发会话内的 select-serial-port 事件
+  win.webContents.session.on('select-usb-device',(event,details,callback)=>{
+    // usb-device-added/removed 两种事件可以处理串口设备的插拔
+    win.webContents.session.on('usb-device-added',(event,device)=>{
+      console.log('usb-device-added Frist width ',device);
+    })
+    win.webContents.session.on('usb-device-removed',(event,device)=>{
+      console.log('usb-device-removed Frist width ',device);
+    })
+    event.preventDefault()
+    if(details.deviceList && details.deviceList.length > 0){
+      const deviceToReturn = details.deviceList.find((device)=>{
+        if(!grantedDeviceThroughPermHandler || (device.deviceId != grantedDeviceThroughPermHandler.deviceId)){
+          return true
+        }
+      })
+      if(deviceToReturn) {
+        callback(deviceToReturn.deviceId)
+      }
+    }else{
+      callback()
+    }
+  })
+
+ 
+  win.webContents.session.setPermissionCheckHandler((webContents,permission,requestingOrigin,details)=>{
+    if(permission === 'usb' && details.securityOrigin === 'file:///'){
+      return true
+    }
+    return false
+  })
+
+  win.webContents.session.setDevicePermissionHandler((details)=>{
+    if(details.deviceType === 'usb' && details.origin === 'file:///'){
+      if(!grantedDeviceThroughPermHandler){
+        grantedDeviceThroughPermHandler = details.device
+        return true
+      }else{
+        return false
+      }
+    }
+    return false
+  })
+
+
+
+  // win.webContents.openDevTools()
   win.loadFile('./index.html')
 }
+
+
+
+
+
+
 app.whenReady().then(()=>{
   createWindow()
   app.on('activate',()=>{
--- electron/day01/example03/renderer.js
+++ electron/day01/example03/renderer.js
@@ -34,3 +34,96 @@ window.electronAPI.bluetoothPairingRequest((event,details)=>{
   }
   window.electronAPI.bluetoothPairingResponse(response)
 })
+
+
+// ********************访问 HID 设备********************
+
+async function testClick(){
+  const grantedDevices = await navigator.hid.getDevices()
+  console.log('grantedDevices===>',grantedDevices);
+  let grantedDeviceList = ''
+  grantedDevices.forEach(device=>{
+    grantedDeviceList+=`<hr>${device.productName}</hr>`
+  })
+  document.querySelector('#granted-devices').innerHTML = grantedDeviceList
+  
+  const grantedDevices2 = await navigator.hid.requestDevice({
+    filters:[]
+  })
+  
+  console.log('grantedDevices2===>',grantedDevices2);
+  grantedDeviceList = ''
+  grantedDevices2.forEach(device=>{
+    grantedDeviceList+=`<hr>${device.productName}</hr>`
+  })
+  document.querySelector('#granted-devices2').innerHTML = grantedDeviceList
+}
+window.addEventListener('DOMContentLoaded',()=>{
+  const btnEleHID = document.querySelector('#btn')
+  btnEleHID.addEventListener('click',testClick)
+})
+
+
+
+// ********************访问串口设备********************
+
+async function  testSerialClick(){
+  const filters = [
+    { usbVendorId: 0x2341, usbProductId: 0x0043 },
+    { usbVendorId: 0x2341, usbProductId: 0x0001 }
+  ];
+
+  try {
+    const port = await navigator.serial.requestPort({filters})
+    const portInfo = port.getInfo()
+    console.log("portInfo=%o",portInfo);
+    document.getElementById('#serial-device-name').innerHTML = `vendorId:${portInfo.usbVendorId} | productId:${portInfo.usbProductId} `
+  } catch (ex) {
+    if(ex.name === 'NotFoundError'){
+      document.getElementById('serial-device-name').innerHTML = `Device NOT found`
+    }else{
+      document.getElementById('serial-device-name').innerHTML = ex
+    }  
+  }
+}
+window.addEventListener('DOMContentLoaded',()=>{
+
+  document.getElementById('btnSerial').addEventListener('click',testSerialClick)
+})
+
+// ********************访问 USB 设备********************
+function getDeviceDetail(device){
+  return device.productName || `Unknown device ${device.deviceId}`
+}
+async function testClickUSB(){
+  const noDevicesFoundMsg = 'No Devices Found'
+
+  const grantedDevices = await navigator.usb.getDevices()
+  let grantedDeviceList = ''
+  if(grantedDevices.length > 0){
+    grantedDevices.forEach(device => {
+      grantedDeviceList +=`<hr>${getDeviceDetail(device)}</hr>`
+    })
+  }else{
+    grantedDeviceList = noDevicesFoundMsg
+  }
+  document.getElementById('granted-devices-usb').innerHTML = grantedDeviceList
+
+  grantedDeviceList = ''
+  try {
+      const grantedDevices = await navigator.usb.requestDevice({
+        filters:[]
+      })
+      grantedDevices.forEach(device=>{
+        grantedDeviceList+=`<hr>${getDeviceDetail(device)}</hr>`
+      })
+      
+  } catch (ex) {
+    if(ex.name === 'NotFoundError'){
+      grantedDeviceList = noDevicesFoundMsg
+    }
+  }
+  document.getElementById('granted-devices2-usb').innerHTML = grantedDeviceList
+}
+
+document.getElementById('clickUSB').addEventListener('click',testClickUSB)
