df7a1afe1b0e40ef5f552281afefba784c352237
zhujinlong <1445824783@qq.com>
2023-03-30 22:41:25 +0800

    倒计时基本样式

---
+++ electron/count-down/main.js
@@ -0,0 +1,27 @@
+const {app,BrowserWindow} = require('electron')
+const createWindow = ()=>{
+  const mainWindow = new BrowserWindow({
+    width:120,
+    height:40,
+    x:1600,
+    y:100,
+    alwaysOnTop:true,
+    frame:false,
+    transparent:true
+  })
+  // mainWindow.webContents.openDevTools()
+  mainWindow.loadFile('index.html')
+}
+app.whenReady().then(()=>{
+  createWindow()
+  app.on('activate',()=>{
+    if(BrowserWindow.getAllWindows().length === 0){
+      createWindow()
+    }
+  })
+})
+app.on('window-all-closed',()=>{
+  if(process.platform !=='darwin'){
+    app.quit()
+  }
+})
---
+++ electron/count-down/renderer.js
@@ -0,0 +1,25 @@
+
+function countDown(time,ele) {
+  const [minutes, seconds] = time.split(':');
+  let remainingTime = (parseInt(minutes) * 60) + parseInt(seconds);
+  const interval = setInterval(() => {
+    remainingTime--;
+    var result = ''
+    if (remainingTime < 0) {
+      const minutes = String(Math.floor(Math.abs(remainingTime) / 60)).padStart(2, '0');
+      const seconds = String(Math.floor(Math.abs(remainingTime) % 60)).padStart(2, '0');
+      result = `-${minutes}:${seconds}`
+    } else {
+      const minutes = String(Math.floor(remainingTime / 60)).padStart(2, '0');
+      const seconds = String(Math.floor(remainingTime % 60)).padStart(2, '0');
+      result = `${minutes}:${seconds}`
+    }
+    ele.innerHTML = result
+  }, 1000);
+}
+
+
+window.addEventListener('DOMContentLoaded',()=>{
+  const countDownEle = document.querySelector('#count-down')
+  countDown('08:00',countDownEle)
+})
